5fdfccf05d0765f51c17f454300f571a0814db41
secdefect <sash@modux.co.uk>
2019-01-22 16:15:03 +0000

    Update times.js

--- times.js
+++ times.js
@@ -1,2 +1,2 @@
 
-$("body").html("<iframe width='100%' height='100%' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://www.thetimes.co.uk'></iframe>");  
+$("body").html("<iframe width='100%' height='1000px' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://www.thetimes.co.uk'></iframe>");  
